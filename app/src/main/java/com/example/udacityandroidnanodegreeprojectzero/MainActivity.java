ce14f385b016175e11699ef0d77a6fab54bacf4c
Inspironsun <me@sunxiaobin.com>
2016-06-02 19:19:41 +0800

    1.use ScrollView for small device and landscape orientation 2.remove hard-coded values and define a style for the 6 similar buttons 3.improve the button onclick events handler

--- app/src/main/java/com/example/udacityandroidnanodegreeprojectzero/MainActivity.java
+++ app/src/main/java/com/example/udacityandroidnanodegreeprojectzero/MainActivity.java
@@ -6,61 +6,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+public class MainActivity extends AppCompatActivity {
 
-    private final int[] mButtonResId = {
-            R.id.movie_button,
-            R.id.stock_button,
-            R.id.bigger_button,
-            R.id.material_button,
-            R.id.ubiq_button,
-            R.id.capstone_button
-    };
-
-    private final String[] mToastMessage = {
-            "Popular Movies",
-            "Stock Hawk",
-            "Make it Bigger",
-            "Make Your App Material",
-            "Go Ubiquitous",
-            "Capstone"
-    };
-
-    private Button[] mButtons = new Button[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getViewsById();
-        bindToast();
     }
 
-    private void getViewsById() {
-        for (int i = 0; i < mButtonResId.length; i++) {
-            mButtons[i] = (Button) findViewById(mButtonResId[i]);
-        }
-    }
-
-    private void bindToast() {
-        for (Button btn : mButtons) {
-            btn.setOnClickListener(this);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        String currentButtonName = "";
-        for (int i = 0; i < mButtons.length; i++) {
-            if (v == mButtons[i]) {
-                currentButtonName = mToastMessage[i];
-            }
-        }
+    public void buttonClick(View v) {
+        Button button = (Button) v;
+        String appName = (String) button.getText();
 
         Toast.makeText(MainActivity.this,
-                "This button will lanuch " + currentButtonName + " app!",
+                "This button will launch " + appName + " app!",
                 Toast.LENGTH_SHORT).show();
     }
+
+
 }
